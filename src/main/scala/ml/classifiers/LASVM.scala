36aa004bff96f237ec0432a12a39f9fb8f4110e4
davips <davips@users.noreply.github.com>
2014-07-28 06:22:29 -0300

    started lasvm and svmHits

---
+++ src/main/scala/ml/classifiers/LASVM.scala
@@ -0,0 +1,36 @@
+package ml.classifiers
+
+import ml.Pattern
+import ml.models.Model
+
+/*
+elm-scala: an implementation of ELM in Scala using MTJ
+Copyright (C) 2014 Davi Pereira dos Santos
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+case class LASVM() extends Learner {
+  override val toString = "LASVM"
+  val notes = ""
+
+  def update(model: Model, fast_mutable: Boolean)(pattern: Pattern) = ???
+
+  def expected_change(model: Model)(pattern: Pattern) = ???
+
+  def EMC(model: Model)(patterns: Seq[Pattern]) = ???
+
+  def build(pool: Seq[Pattern]) = {
+    ???
+  }
+}
