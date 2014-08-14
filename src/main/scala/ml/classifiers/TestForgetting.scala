0112cfae9cb70c43257f82ac00e5e846f35b85d6
davips <davips@users.noreply.github.com>
2014-08-14 04:10:31 -0300

    sql debug

--- src/main/scala/ml/classifiers/TestForgetting.scala
+++ src/main/scala/ml/classifiers/TestForgetting.scala
@@ -28,27 +28,63 @@ object TestForgetting extends App {
 
   val lh = VFDT() //can forget
 
-  val lk = KNNinc(5, "eucl", df) //KNN cannot forget
+  //  val lk = KNNinc(5, "eucl", df) //KNN cannot forget
 
   var n = ln.build(df.take(df.head.nclasses))
   var h = lh.build(df.take(df.head.nclasses))
-  var k = lk.build(df.take(df.head.nclasses))
   val fast_mutable = true
 
-  df.drop(df.head.nclasses).foreach { p =>
+  val l0 = df.drop(df.head.nclasses).map { p =>
     n = ln.update(n, fast_mutable)(p)
     h = lh.update(h, fast_mutable)(p)
-    k = lk.update(k, fast_mutable)(p)
-    println(s"updating ${n.accuracy(df)} ${h.accuracy(df)} ${k.accuracy(df)}")
+    (n.accuracy(df), h.accuracy(df))
   }
-  val list = df.drop(df.head.nclasses).reverse.map { p =>
+  val l1 = df.drop(df.head.nclasses).reverse.map { p =>
     val np = p.relabeled_reweighted(p.label, -p.weight(), new_missed = false)
     n = ln.update(n, fast_mutable)(np)
     h = lh.update(h, fast_mutable)(np)
-    k = lk.update(k, fast_mutable)(np)
-    (n.accuracy(df), h.accuracy(df), k.accuracy(df))
+    (n.accuracy(df), h.accuracy(df))
+  }.reverse
+  println(s"nb vfdt nb- vfdt-")
+  l0.zip(l1).foreach { case ((nb0, vf0), (nb1, vf1)) =>
+    println(s"$nb0 $vf0 $nb1 $vf1 ")
   }
-  list.reverse.foreach { case (a, b, c) =>
-    println(s"forgetting reversed $a $b $c")
+  //  list.reverse.foreach { case (a, b, c) =>
+  //    println(s"forgetting reversed $a $b $c")
+  //  }
+}
+
+object TestSingleForgetting extends App {
+  val d = Datasets.arff(bina = true)("/home/davi/wcs/ucipp/uci/abalone-11class.arff", zscored = false).right.get.toList.take(500)
+  val f = Datasets.zscoreFilter(d)
+  val df = Datasets.applyFilterChangingOrder(d, f)
+  val dtr = Datasets.applyFilterChangingOrder(d, f).take(250)
+  val dts = Datasets.applyFilterChangingOrder(d, f).drop(250)
+  val fast_mutable = true
+
+  //normal
+  val ln = NB()
+  val lh = VFDT()
+  var n = ln.build(dtr.take(dtr.head.nclasses))
+  var h = lh.build(dtr.take(dtr.head.nclasses))
+  val l0 = dtr.drop(dtr.head.nclasses).map { p =>
+    n = ln.update(n, fast_mutable)(p)
+    h = lh.update(h, fast_mutable)(p)
+    (n.accuracy(dts), h.accuracy(dts))
+  }
+
+  //põe-tira
+  var n2 = ln.build(dtr.take(dtr.head.nclasses))
+  var h2 = lh.build(dtr.take(dtr.head.nclasses))
+
+  val l1 = dtr.drop(dtr.head.nclasses).map { p =>
+
+    n2 = ln.update(n2, fast_mutable)(p)
+    h2 = lh.update(h2, fast_mutable)(p)
+    (n2.accuracy(dts), h2.accuracy(dts))
+  }
+
+  l0.zip(l1).foreach { case ((a0, a1), (b0, b1)) =>
+    println(s"$a0 $b0 $a1 $b1 ")
   }
 }
