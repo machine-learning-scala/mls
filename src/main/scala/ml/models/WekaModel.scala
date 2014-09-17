fe3aa34eba0027b6b9771178b8a07c7d3e8aa120
davips <davips@users.noreply.github.com>
2014-09-17 19:18:50 -0300

    teste q e h

--- src/main/scala/ml/models/WekaModel.scala
+++ src/main/scala/ml/models/WekaModel.scala
@@ -31,7 +31,10 @@ class WekaModel(var classifier: Classifier) extends Model {
 
   def predict(instance: Pattern) =
     if (classifier == null) throw new Exception("Underlying model already changed! Please call update with fast_mutable disabled.")
-    else classifier.classifyInstance(instance)
+    else {
+      //      println(instance + " <<<<<<<<<<<<<")
+      classifier.classifyInstance(instance)
+    }
 }
 
 case class WekaBatModel(private val batch_classifier: Classifier, training_set: Seq[Pattern]) extends WekaModel(batch_classifier)
