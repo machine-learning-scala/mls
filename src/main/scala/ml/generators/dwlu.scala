668e67ff40da83a4b6ce83a0bb0473ae896bc398
davips <davips@users.noreply.github.com>
2014-12-06 04:34:00 -0200

    dwtu ploter corrigido

--- src/main/scala/ml/generators/dwlu.scala
+++ src/main/scala/ml/generators/dwlu.scala
@@ -19,46 +19,50 @@
 package ml.generators
 
 object dwlu extends App {
+   //   def f (x:Double) =math.sqrt(x)
+   def f(x: Double) = 1 - 1 / (1 + math.sqrt(x))
+
   for (x <- -10 to 10 map (_ / 10d); y <- -10 to 10 map (_ / 10d)) {
     val d0 = {
       val dx = 1 - x
       val dy = 1 - y
-      math.sqrt(dx * dx + dy * dy)
+       f(dx * dx + dy * dy)
     }
     val d1 = {
       val dx = 0.75 - x
       val dy = 1 - y
-      math.sqrt(dx * dx + dy * dy)
+       f(dx * dx + dy * dy)
     }
     val d2 = {
       val dx = 0.75 - x
       val dy = 0.75 - y
-      math.sqrt(dx * dx + dy * dy)
+       f(dx * dx + dy * dy)
     }
 
     val d0b = {
       val dx = -1 - x
       val dy = -1 - y
-      math.sqrt(dx * dx + dy * dy)
+       f(dx * dx + dy * dy)
     }
     val d1b = {
       val dx = -0.75 - x
       val dy = -1 - y
-      math.sqrt(dx * dx + dy * dy)
+       f(dx * dx + dy * dy)
     }
     val d2b = {
       val dx = -0.75 - x
       val dy = -0.75 - y
-      math.sqrt(dx * dx + dy * dy)
+       f(dx * dx + dy * dy)
     }
 
     val d00 = {
       val dx = 0 - x
       val dy = 0 - y
-      math.sqrt(dx * dx + dy * dy)
+       f(dx * dx + dy * dy)
     }
 
-    val d = Seq(d0, d1, d2, d0b, d1b, d2b, d00).sorted.take(4).sum
+     val d = Seq(d0, d1, d2, d0b, d1b, d2b, d00).sorted.sum
+     //    val d = Seq(d0, d1, d2, d0b, d1b, d2b, d00).sorted.take(4).sum
     println(s"$x $y $d")
   }
 }
