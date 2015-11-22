b218b4ff23f57175aea397ac57e1a0a8846474b4
davips <davips@users.noreply.github.com>
2015-11-22 18:09:49 -0200

    update na geração de resultados

--- src/main/scala/ml/classifiers/C45.scala
+++ src/main/scala/ml/classifiers/C45.scala
@@ -25,6 +25,7 @@ import ml.models.{Model, WekaModel}
 import util.Datasets
 import weka.classifiers.Classifier
 import weka.classifiers.trees.J48
+
 import scala.util.parsing.combinator.{ImplicitConversions, JavaTokenParsers, RegexParsers}
 
 case class C45(laplace: Boolean = true, minobjs: Int = -1, explicitos: Double = 1) extends BatchWekaLearner {
@@ -96,7 +97,7 @@ case class C45(laplace: Boolean = true, minobjs: Int = -1, explicitos: Double = 
           x._2 == bef.last._2 && (explicitos == 1 || x._2 > 1)
         })
         val demais = tot - qtds2.map(_._2).sum
-        val qtds3 = if (demais > 0) qtds2 ++ aft.tail.map(x => "% " + x._1 -> x._2) :+ ("demais ($\\leq " + srtd.drop(qtds2.size).head._2 + "$)" -> demais) else qtds2
+        val qtds3 = if (demais > 0) qtds2 ++ aft.tail.map(x => "% " + x._1 -> x._2) :+ ("demais ($\\leq " + aft.tail.head._2 + "$)" -> demais) else qtds2
         "child {node [outcome] {\n" + qtds3.map(x => x._1 + ": " + x._2).mkString("\\\\\n") + "} edge from parent node [cond] {" + op(operador, valor) + "}}"
       } else "%"
     case _ => sys.error(s"erro matching")
@@ -122,7 +123,7 @@ case class C45(laplace: Boolean = true, minobjs: Int = -1, explicitos: Double = 
         println(s"")
         println(str)
         println(s"")
-        val r = trav(Parsing.parse(str)).replace("maiorque", ">").replace("simbolomenor", "<") + "\n;"
+        val r = trav(Parsing.parse(str)).replace("Clu", "HS").replace("C4.52", "C4.5w").replace("maiorque", ">").replace("simbolomenor", "<") + "\n;"
         if (tex != null) {
           val fw2 = new PrintWriter(tex, "UTF-8")
           fw2.write(r)
